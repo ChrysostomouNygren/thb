2c366f9106f753bc287f02322635d8230766974c
Katerina Chrysostomou Nygren <katerina.chrysostomou@hotmail.com>
2023-01-17 15:22:56 +0100

    scrollbar and styling for it

--- src/components/discography/Discography.jsx
+++ src/components/discography/Discography.jsx
@@ -3,66 +3,54 @@ import axios from "axios";
 import "./Discography.scss";
 
 function Discography() {
-  const [disc, setDisc] = useState([]);
-  const [tracks, setTracks] = useState([]);
+  const [realeses, setRealeses] = useState([]);
   const [track, setTrack] = useState([]);
-  const [ids, setIds] = useState([]);
-  const getDicogs = () => {
+
+
+  const getReleases = () => {
     axios
       .get("https://api.discogs.com/artists/7427346/releases")
       .then((response) => {
         const result = response.data.releases;
-        // console.log(result);
-        setDisc(result);
-        const getId = result.map(({ id }) => id);
-        // console.log(getId);
-        setIds(getId);
+        setRealeses(result);
       });
-  };
-
-  // inte helt övergivit detta skepp... hade varit soft att kunna rendera ut låtarna konstant med varje skiva och inte var för sig.
-  const getTracks = () => {
-    ids.map((id) =>
-      axios.get(`https://api.discogs.com/releases/${id}`).then((response) => {
-        const result = response.data;
-        // console.log(result.id, result.tracklist);
-        setTracks((...prev) => [...prev, result]);
-      })
-    );
   };
 
   const getTrack = (id) => {
     axios.get(`https://api.discogs.com/releases/${id}`).then((response) => {
-      console.log(response.data);
       setTrack(response.data);
     });
   };
 
   useEffect(() => {
-    getDicogs();
-    getTracks();
+    getReleases();
   }, []);
 
   return (
     <div className="disc-comp">
       <h2>Discography</h2>
-      {disc.map((record) => (
+      {realeses.map((record) => (
         <div key={record.id} className="disc-card">
-          <h3>{record.title}</h3>
+          <div className="album-title">
+            <h3>{record.title}</h3>
+            <p>{record.year}</p>
+          </div>
           <div className="rendered-card">
             <div className="rendered-disc">
-              <p>{record.year}</p>
               <p>Label: {record.label}</p>
               <p>Format: {record.format}</p>
             </div>
-            {/* <p>{record.id}</p> */}
             {/* låttitelrendering */}
-            <div className="rendered-tracks">
-              <button onClick={() => getTrack(record.id)}>Track list</button>
+            <div
+              className={
+                track.id === record.id ? "scroll-tracks" : "rendered-tracks"
+              }
+            >
+              {/* ge knapp klassnamn för att göra osynlig vid aktivering */}
+              <button onClick={() => getTrack(record.id)}>{track.id === record.id ? '' : 'Track list'}</button>
               {track.id === record.id ? (
                 track.tracklist.map((song) => (
                   <div key={song.title} className="rendered-track">
-                    {/* {song.artists[0].name ? <p>{song.artists[0].name}</p> : <></>} */}
                     <p>{song.title}</p>
                     <p>{song.duration}</p>
                   </div>
