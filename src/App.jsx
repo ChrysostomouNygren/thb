6da22b3373722c89b896f3e69f889da10508785c
Katerina Chrysostomou Nygren <katerina.chrysostomou@hotmail.com>
2023-01-18 14:00:19 +0100

    added contact to navbar

--- src/App.jsx
+++ src/App.jsx
@@ -3,20 +3,25 @@ import "./App.css";
 import Mediaplayer from "./components/mediaplayer/Mediaplayer";
 import Discography from "./components/discography/Discography";
 // import Navbar from "./components/navbar/Navbar";
-import './components/navbar/Navbar.scss'
+import "./components/navbar/Navbar.scss";
 import Links from "./components/links/Links";
 
 function App() {
   const handleClick = (id) => {
     if (id === "links") {
-      const element = document.querySelector('#links');
+      const element = document.querySelector("#links");
       element.scrollIntoView({ behavior: "smooth" });
-      console.log('success to links')
+      console.log("success to links");
     }
     if (id === "discography") {
-      const element = document.querySelector('#discography')
+      const element = document.querySelector("#discography");
       element.scrollIntoView({ behavior: "smooth" });
-      console.log('success to discography')
+      console.log("success to discography");
+    }
+    if (id === "contact") {
+      const element = document.querySelector("#contact");
+      element.scrollIntoView({ behavior: "smooth" });
+      console.log("success to contact");
     }
   };
   return (
@@ -25,14 +30,14 @@ function App() {
       {/* <Navbar /> */}
       <div className="navbar">
         <div className="navbar-center">
-
-        <h3 onClick={() => handleClick("links")}>Links</h3>
-        <h3 onClick={() => handleClick("discography")}>Discography</h3>
+          <h3 onClick={() => handleClick("links")}>Links</h3>
+          <h3 onClick={() => handleClick("discography")}>Discography</h3>
+          <h3 onClick={() => handleClick("contact")}>Contact</h3>
         </div>
       </div>
       <Mediaplayer />
       <Links />
-      <Discography/>
+      <Discography />
     </div>
   );
 }
