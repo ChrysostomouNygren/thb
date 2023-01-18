0ef0029bf3615159538259dc356347d7a03a5d82
Katerina Chrysostomou Nygren <katerina.chrysostomou@hotmail.com>
2023-01-18 09:56:19 +0100

    lots of styling

--- src/components/discography/Discography.jsx
+++ src/components/discography/Discography.jsx
@@ -6,7 +6,6 @@ function Discography() {
   const [realeses, setRealeses] = useState([]);
   const [track, setTrack] = useState([]);
 
-
   const getReleases = () => {
     axios
       .get("https://api.discogs.com/artists/7427346/releases")
@@ -29,39 +28,44 @@ function Discography() {
   return (
     <div className="disc-comp">
       <h2>Discography</h2>
-      {realeses.map((record) => (
-        <div key={record.id} className="disc-card">
-          <div className="album-title">
-            <h3>{record.title}</h3>
-            <p>{record.year}</p>
-          </div>
-          <div className="rendered-card">
-            <div className="rendered-disc">
-              <p>Label: {record.label}</p>
-              <p>Format: {record.format}</p>
+      <div className="disc-border">
+        {realeses.map((record) => (
+          <div key={record.id} className="disc-card">
+            <div className="album-title">
+              <h3>{record.title}</h3>
+              <p>{record.year}</p>
             </div>
-            {/* låttitelrendering */}
-            <div
-              className={
-                track.id === record.id ? "scroll-tracks" : "rendered-tracks"
-              }
-            >
-              {/* ge knapp klassnamn för att göra osynlig vid aktivering */}
-              <button onClick={() => getTrack(record.id)}>{track.id === record.id ? '' : 'Track list'}</button>
-              {track.id === record.id ? (
-                track.tracklist.map((song) => (
-                  <div key={song.title} className="rendered-track">
-                    <p>{song.title}</p>
-                    <p>{song.duration}</p>
-                  </div>
-                ))
-              ) : (
-                <></>
-              )}
+            <div className="rendered-card">
+              <div className="rendered-disc">
+                <p>Label: {record.label}</p>
+                <p>Format: {record.format}</p>
+              </div>
+              {/* låttitelrendering */}
+              <div
+                className={
+                  track.id === record.id ? "scroll-tracks" : "rendered-tracks"
+                }
+              >
+                {/* ge knapp klassnamn för att göra osynlig vid aktivering */}
+                <button onClick={() => getTrack(record.id)}>
+                  {track.id === record.id ? "" : "Track list"}
+                </button>
+                {track.id === record.id ? (
+                  track.tracklist.map((song) => (
+                    <div key={song.title} className="rendered-track">
+                      <p>{song.title}</p>
+                      <p>{song.duration}</p>
+                    </div>
+                  ))
+                ) : (
+                  <></>
+                )}
+              </div>
             </div>
+            <hr />
           </div>
-        </div>
-      ))}
+        ))}
+      </div>
     </div>
   );
 }
