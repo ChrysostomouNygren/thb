af29e033b6370ad3324d38c0f3111898c98820d6
Katerina Chrysostomou Nygren <katerina.chrysostomou@hotmail.com>
2023-01-18 14:14:06 +0100

    styled contacts

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -2,7 +2,19 @@ import React from "react";
 import "./Contact.scss";
 
 function Contact() {
-  return <div id="contact">Contact</div>;
+  return (
+    <div className="contact-comp" id="contact">
+      <h2>Contact</h2>
+      <div className="contact-border">
+        <div className="contact-card">
+          <a href="https://www.instagram.com/the.horizon.bleeds/">
+              Instagram
+            </a>
+            <a href="mailto:#">Email</a>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Contact;
