bb00b65826244e34e7d1106b5d6fc15fc5f2ac01
Katerina Chrysostomou Nygren <katerina.chrysostomou@hotmail.com>
2023-01-18 13:57:48 +0100

    added links and some styling

--- README.md
+++ README.md
@@ -12,3 +12,9 @@ https://api.discogs.com/artists/7427346/releases
 17/1 - tracklist äntligen implementerad. Tog orimligt mycket längre tid än vad det egentligen borde gjort. Jag är antagligen dum i huvuet.
 * VA-skivorna, bara rendera ut THB-låten? Få ut artistnamnen iaf!
 * scroll på de längre listorna
+
+18/7 - todo:
+* to-the-top knapp tack
+* contact: email, instagram
+* grid för links
+* optimera för galaxy fold: 280px (mindre än 375px bredd)
--- src/components/links/Links.jsx
+++ src/components/links/Links.jsx
@@ -1,9 +1,36 @@
-import React from 'react'
+import React from "react";
+import "./Links.scss";
 
 function Links() {
   return (
-    <div>Links</div>
-  )
+    <div className="links-comp" id="links">
+      <h2>Links</h2>
+      <div className="links-border">
+        <div className="links-card">
+          <a
+            href="https://open.spotify.com/artist/1slNQIXSNeYaxLKmpJvtaJ
+            "
+          >
+            Spotify
+          </a>
+          <a
+            href="https://music.apple.com/us/artist/the-horizon-bleeds/1438543866
+            "
+          >
+            Apple music
+          </a>
+
+          <a href="https://thehorizonbleeds.bandcamp.com/">Bandcamp</a>
+          <a href="https://www.discogs.com/artist/7427346-The-Horizon-Bleeds">
+            Discogs
+          </a>
+          {/* <a href="https://www.instagram.com/the.horizon.bleeds/">
+              Instagram
+            </a> */}
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default Links
+export default Links;
