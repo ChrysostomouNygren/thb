1b43dbfc12180477dc691d71a32597721e07e62c
Katerina Chrysostomou Nygren <katerina.chrysostomou@hotmail.com>
2023-01-18 11:19:29 +0100

    simple navbar

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,13 @@ import Header from "./components/header/Header";
 import "./App.css";
 import Mediaplayer from "./components/mediaplayer/Mediaplayer";
 import Discography from "./components/discography/Discography";
+import Navbar from "./components/navbar/Navbar";
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Navbar />
       <Mediaplayer />
       <Discography />
     </div>
---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Navbar.scss'
+
+function Navbar() {
+  return (
+    <div className='navbar'>
+        <h3>Links</h3>
+        <h3>Discography</h3>
+        <h3>Contact</h3>
+    </div>
+  )
+}
+
+export default Navbar
